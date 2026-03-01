37f761463d0db47b4c8d4379398e1518134903b1
Yusuke Shibata <yusukeshib@gmail.com>
2026-03-01 08:12:20 -0500

    fmt

--- src/mux/client.rs
+++ src/mux/client.rs
@@ -324,11 +324,7 @@ fn draw_sidebar(f: &mut ratatui::Frame, sidebar: &SidebarState, area: Rect) {
             if x < buf.area().width && row_y < buf.area().height {
                 let cell = &mut buf[(x, row_y)];
                 cell.set_symbol(" ");
-                cell.set_style(if is_selected {
-                    fg_style
-                } else {
-                    bg_style
-                });
+                cell.set_style(if is_selected { fg_style } else { bg_style });
             }
         }
         // Write text (not dimmed for active entries)
